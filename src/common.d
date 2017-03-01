b3ab94db95b64f47a2446b10ee86a6d4924cfa24
Johannes Pfau <johannespfau@gmail.com>
2017-03-02 00:30:16 +0100

    Do not log git commands to stdout

--- src/common.d
+++ src/common.d
@@ -40,39 +40,39 @@ struct OutConfig
 void setupRepo(Source source)
 {
     if (!exists(repoDir))
-        run("git clone " ~ source.uri ~ " " ~ repoDir);
+        runCollect("git clone " ~ source.uri ~ " " ~ repoDir);
     else
     {
         chdir(repoDir);
-        run("git fetch origin");
+        runCollect("git fetch origin");
     }
 
     chdir(repoDir);
-    run("git reset --hard origin/master");
+    runCollect("git reset --hard origin/master");
 }
 
 void setupAuth(Source source)
 {
     std.file.write(privateKeyPath, source.private_key);
-    run(`chmod 600 ` ~ privateKeyPath);
+    runCollect(`chmod 600 ` ~ privateKeyPath);
     environment["GIT_SSH_COMMAND"] = "ssh -o StrictHostKeyChecking=no -i " ~ privateKeyPath;
 }
 
 void checkoutVersion(Version ver)
 {
-    run("git checkout " ~ ver.gitRef);
+    runCollect("git checkout " ~ ver.gitRef);
 }
 
 string commitPushRepo(Source source)
 {
     if (!source.git_name.empty)
-        run(`git config --global user.name ` ~ source.git_name);
+        runCollect(`git config --global user.name ` ~ source.git_name);
     if (!source.git_email.empty)
-        run(`git config --global user.email ` ~ source.git_email);
+        runCollect(`git config --global user.email ` ~ source.git_email);
 
-    run(`git add *`);
-    run(`git commit -m "Add new version for ` ~ source.file ~ `"`);
-    run(`git push`);
+    runCollect(`git add *`);
+    runCollect(`git commit -m "Add new version for ` ~ source.file ~ `"`);
+    runCollect(`git push`);
     return runCollect(`git rev-parse HEAD`).chomp();
 }
     
